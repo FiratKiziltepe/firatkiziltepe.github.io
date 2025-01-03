b46357226f7fe652ec793205e8b9fded17622aae
FIRAT KIZILTEPE <24810201001@gazi.edu.tr>
2025-01-03 16:22:31 +0300

    tork simülasyonu
    tork simülasyonu

---
+++ tork/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhysicsSimulation from './components/PhysicsSimulation';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <PhysicsSimulation />
+    </div>
+  );
+}
+
+export default App;
