f264be74f616ed12808c0f4e9baa4aa0e4953b13
FIRAT KIZILTEPE <24810201001@gazi.edu.tr>
2025-07-01 15:05:37 +0300

    Update vite.config.js

--- ethic/vite.config.js
+++ ethic/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ethic/', // <-- projenin adı neyse onunla aynı olmalı
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
